c623073ac020cc35664fdc9b48fbaaff5b327ad3
Natte Vagon <nattevagon@gmail.com>
2025-02-06 15:24:02 +0700

    [FE] Fixing Error #Natte

--- src/pages/api/clubs.js
+++
@@ -1,6 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import clubs from './data/clubs.json'
-
-export default function handler(req, res) {
-  res.status(200).json(clubs);
-}
